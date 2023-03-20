c9134a70a98bc015746751317a2f6458f4727612
Tom G <Tom@admins-MacBook-Pro.local>
2023-03-20 15:20:17 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+todays_soup = "Tomato"
+soup_price = 5
+bread_price = 2
+deal_price = soup_price + bread_price - 1
+
+print("soup of the day is " + todays_soup)
+print("price of soup is £" + str(soup_price))
+print("price of bread is £" + str(bread_price)) 
+print("Todays meal deal price is £" + str(deal_price))
